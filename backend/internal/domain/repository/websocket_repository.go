1d484ec832ade4f7bbae6547307011a8c532acb7
nakamuraitsuki <ituki1911n@gmail.com>
2025-04-15 23:42:54 +0900

    feat(domain/repository): 一旦チャット用っぽく変更

--- backend/internal/domain/repository/websocket_repository.go
+++ backend/internal/domain/repository/websocket_repository.go
@@ -1,12 +1,10 @@
 package repository
 
+import "example.com/webrtc-practice/internal/domain/entity"
+
 type IWebsocketRepository interface {
-	CreateClient(id string) error
-	DeleteClient(id string) error
-	SaveSDP(id string, sdp string) error
-	GetSDPByID(id string) (string, error)
-	SaveCandidate(id string, candidate []string) error
-	AddCandidate(id string, candidate []string) error
-	ExistsCandidateByID(id string) bool
-	GetCandidatesByID(id string) ([]string, error)
+	CreateClient(id entity.WebsocketClientID) error
+	DeleteClient(id entity.WebsicketClientID) error
+	GetClientByID(id entity.WebsicketClientID) (*entity.WebsocketClient, error)
+	GetClientsByRoomID(roomID entity.RoomID) ([]*entity.WebsocketClient, error)
 }
