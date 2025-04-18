0abcc257bb28db55ba22db18a86ebf2a46b8fadb
nakamuraitsuki <ituki1911n@gmail.com>
2025-04-18 19:52:04 +0900

    fix(domain): 型がバラけてしまったので、Entity->ポインタ、ID系->ラッパー、に統一

--- backend/internal/domain/repository/user_repository.go
+++ backend/internal/domain/repository/user_repository.go
@@ -7,8 +7,8 @@ import (
 type UserRepository interface {
 	SaveUser(*entity.User) (*entity.User, error)
 	GetAllUsers() ([]*entity.User, error)
-	GetUserByID(id int) (*entity.User, error)
+	GetUserByID(id entity.UserID) (*entity.User, error)
 	GetUserByEmail(email string) (*entity.User, error)
 	UpdateUser(user *entity.User) (*entity.User, error)
-	DeleteUser(id int) error
+	DeleteUser(id entity.UserID) error
 }
--- backend/internal/domain/repository/websocket_client_repository.go
+++ backend/internal/domain/repository/websocket_client_repository.go
@@ -3,8 +3,9 @@ package repository
 import "example.com/webrtc-practice/internal/domain/entity"
 
 type WebsocketClientRepository interface {
-	CreateClient(entity.WebsocketClient) error
+	CreateClient(client *entity.WebsocketClient) error
 	DeleteClient(id entity.WebsocketClientID) error
 	GetClientByID(id entity.WebsocketClientID) (*entity.WebsocketClient, error)
 	GetClientsByRoomID(roomID entity.RoomID) ([]*entity.WebsocketClient, error)
+	GetClientsByUserID(userID entity.UserID) (*entity.WebsocketClient, error)
 }
