6026354a2cc514cfe96cfeb73b16481506735bd2
nakamuraitsuki <ituki1911n@gmail.com>
2025-04-20 23:39:07 +0900

    feat(routes): route制定

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,21 +2,29 @@ package routes
 
 import (
 	"example.com/webrtc-practice/config"
+	"example.com/webrtc-practice/internal/interface/handler"
 
 	"github.com/labstack/echo/v4"
 )
 
 func SetupRoutes(
-	e *echo.Echo, 
-	cfg *config.Config, 
-	// userHandler handler.UserHandler,
-	// websocketHandler handler.WebsocketHandler,
+	e *echo.Echo,
+	cfg *config.Config,
+	userHandler handler.UserHandler,
+	websocketHandler handler.WebSocketHandler,
+	messageHandler handler.MessageHandler,
+	roomHandler handler.RoomHandler,
 ) {
 
-	// userGroup := e.Group("/api/user")
-	// userHandler.Register(userGroup)
-
-	// // Websocket
-	// websocketGroup := e.Group("/ws")
-	// websocketHandler.Register(websocketGroup)
+	userGroup := e.Group("/api/user")
+	userHandler.Register(userGroup)
+	// Websocket
+	websocketGroup := e.Group("/ws")
+	websocketHandler.Register(websocketGroup)
+	// Message
+	messageGroup := e.Group("/api/message")
+	messageHandler.Register(messageGroup)
+	// Room
+	roomGroup := e.Group("/api/room")
+	roomHandler.Register(roomGroup)
 }
