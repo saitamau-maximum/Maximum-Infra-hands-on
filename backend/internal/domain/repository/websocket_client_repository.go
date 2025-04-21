fdd96296244e9a2b1f5f244b9b5110e70d885976
nakamuraitsuki <isukin@gmail.com>
2025-04-21 15:54:55 +0900

    fix: repositoryを対応

--- backend/internal/domain/repository/websocket_client_repository.go
+++ backend/internal/domain/repository/websocket_client_repository.go
@@ -4,8 +4,8 @@ import "example.com/webrtc-practice/internal/domain/entity"
 
 type WebsocketClientRepository interface {
 	CreateClient(client *entity.WebsocketClient) error
-	DeleteClient(id entity.WebsocketClientID) error
-	GetClientByID(id entity.WebsocketClientID) (*entity.WebsocketClient, error)
+	DeleteClient(id entity.WsClientID) error
+	GetClientByID(id entity.WsClientID) (*entity.WebsocketClient, error)
 	GetClientsByRoomID(roomID entity.RoomID) ([]*entity.WebsocketClient, error)
 	GetClientsByUserID(userID entity.UserID) (*entity.WebsocketClient, error)
 }
