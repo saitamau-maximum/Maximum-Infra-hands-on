98cb7d6d7640ce48d627cd63a579fc11f3985903
nakamuraitsuki <isukin@gmail.com>
2025-04-21 16:07:58 +0900

    fix(factory): IDFactoryを修正

--- backend/internal/interface/factory/id_factory.go
+++ backend/internal/interface/factory/id_factory.go
@@ -3,26 +3,25 @@ package factory
 import "example.com/webrtc-practice/internal/domain/entity"
 
 type UserIDFactory interface {
-	NewUserID() (entity.UserID, error)
-	FromString(string) entity.UserID
+	NewUserPublicID() (entity.UserPublicID, error)
+	FromInt(int) entity.UserID
+	FromString(string) entity.UserPublicID
 }
 
 type RoomIDFactory interface {
-	NewRoomID() (entity.RoomID, error)
-	FromInt(int) entity.RoomID
-}
-
-type RoomPublicIDFactory interface {
 	NewRoomPublicID() (entity.RoomPublicID, error)
+	FromInt(int) entity.RoomID
 	FromString(string) entity.RoomPublicID
 }
 
 type MessageIDFactory interface {
-	NewMessageID() (entity.MessageID, error)
-	FromString(string) entity.MessageID
+	NewMessagePublicID() (entity.MessagePublicID, error)
+	FromString(string) entity.MessagePublicID
+	FromInt(int) entity.MessageID
 }
 
-type WebsocketClientIDFactory interface {
-	NewWebsocketClientID() (entity.WebsocketClientID, error)
-	FromString(string) entity.WebsocketClientID
+type WebsocketIDFactory interface {
+	NewWsClientPublicID() (entity.WsClientPublicID, error)
+	FromInt(int) entity.WsClientID
+	FromString(string) entity.WsClientPublicID
 }
