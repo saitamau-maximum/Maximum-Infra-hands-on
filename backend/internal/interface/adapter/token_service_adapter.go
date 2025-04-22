fd9c177125d4eeeb0937d452a01e9030157e0fa9
nakamuraitsuki <isukin@gmail.com>
2025-04-22 13:58:39 +0900

    feat: add token service impl and test

--- backend/internal/interface/adapter/token_service_adapter.go
+++ backend/internal/interface/adapter/token_service_adapter.go
@@ -3,6 +3,6 @@ package adapter
 import "example.com/webrtc-practice/internal/domain/entity"
 
 type TokenServiceAdapter interface {
-	GenerateToken(userID entity.UserID) (string, error)
+	GenerateToken(userPublicID entity.UserPublicID) (string, error)
 	ValidateToken(token string) (int, error)
 }
