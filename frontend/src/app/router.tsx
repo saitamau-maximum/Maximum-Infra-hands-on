64f776cd335d0fcbd44a1dc4bfe0b6a470ba62ce
nakamuraitsuki <ituki1911n@gmail.com>
2025-04-23 22:47:32 +0900

    feat(frontend): header layout

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,10 +1,17 @@
 // src/app/router.tsx
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "../features/home";
+import { Layout } from "../shared/components/Layout/Layout";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      }
+    ]
   }
 ]);
