a0e011dbe1330b3232564f052d8c5ce5348b4c16
nakamuraitsuki <ituki1911n@gmail.com>
2025-04-25 00:16:05 +0900

    chore: delete Header

--- frontend/src/shared/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Header.module.css'
-
-export const Header = () => {
-  return (
-    <div className={styles.header}>
-      <p className={styles.hero}>Chat-INFRA</p>
-    </div>
-  )
-}
