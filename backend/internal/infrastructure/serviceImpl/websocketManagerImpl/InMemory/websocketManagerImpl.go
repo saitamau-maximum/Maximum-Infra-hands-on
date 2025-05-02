7ae74a69ecd8df291647e988456c7475a87af449
nakamuraitsuki <ituki1911n@gmail.com>
2025-05-02 23:01:24 +0900

    chore: log消し

--- backend/internal/infrastructure/serviceImpl/websocketManagerImpl/InMemory/websocketManagerImpl.go
+++ backend/internal/infrastructure/serviceImpl/websocketManagerImpl/InMemory/websocketManagerImpl.go
@@ -2,7 +2,6 @@ package inmemorywsmanagerimpl
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 
 	"example.com/infrahandson/internal/domain/entity"
@@ -25,14 +24,13 @@ func NewInMemoryWebSocketManager() service.WebsocketManager {
 	return &InMemoryWebSocketManager{
 		connectionsByUser: make(map[entity.UserID]service.WebSocketConnection),
 		connectionsByRoom: make(map[entity.RoomID]map[entity.UserID]service.WebSocketConnection),
-		idByConn:			 make(map[service.WebSocketConnection]IDs),
+		idByConn:          make(map[service.WebSocketConnection]IDs),
 	}
 }
 
 func (m *InMemoryWebSocketManager) Register(conn service.WebSocketConnection, userID entity.UserID, roomID entity.RoomID) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	fmt.Println("\nRegistering connection for userID:", userID, "roomID:", roomID)
 	// ユーザーごとの接続を登録
 	m.connectionsByUser[userID] = conn
 
@@ -56,12 +54,11 @@ func (m *InMemoryWebSocketManager) Unregister(conn service.WebSocketConnection) 
 	if !exists {
 		return errors.New("connection not found")
 	}
-	fmt.Println("\nUnregistering connection for userID:", IDs.UserID, "roomID:", IDs.RoomID)
 
 	delete(m.idByConn, conn)
 	delete(m.connectionsByUser, IDs.UserID)
 	delete(m.connectionsByRoom[IDs.RoomID], IDs.UserID)
-	
+
 	return nil
 }
 
@@ -85,10 +82,7 @@ func (m *InMemoryWebSocketManager) BroadcastToRoom(roomID entity.RoomID, msg *en
 		return errors.New("room not found")
 	}
 
-	fmt.Println("Broadcast content:", msg.GetContent())
-
-	for id, conn := range users {
-		fmt.Println("Broadcasting to userID:", id)
+	for _, conn := range users {
 		if err := conn.WriteMessage(msg); err != nil {
 			return err
 		}
