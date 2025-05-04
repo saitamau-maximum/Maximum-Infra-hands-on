50402a4d02f243ab7f7f7e37d6b1afd93d7e09dc
nakamuraitsuki <ituki1911n@gmail.com>
2025-05-04 18:55:26 +0900

    fix: adapterからPublicIDを削除

--- backend/internal/interface/adapter/tokenServiceAdapter.go
+++ backend/internal/interface/adapter/tokenServiceAdapter.go
@@ -3,7 +3,7 @@ package adapter
 import "example.com/infrahandson/internal/domain/entity"
 
 type TokenServiceAdapter interface {
-	GenerateToken(userPublicID entity.UserPublicID) (string, error)
+	GenerateToken(userID entity.UserID) (string, error)
 	ValidateToken(token string) (string, error)
 	GetExpireAt(token string) (int, error)
 }
