eab59d366479c926c52e75f43d0a0d105b76ca49
nakamuraitsuki <ituki1911n@gmail.com>
2025-05-04 18:53:21 +0900

    fix: repositoryからPublicIDを削除（uuidに統一のため）

--- backend/internal/domain/repository/userRepository.go
+++ backend/internal/domain/repository/userRepository.go
@@ -6,6 +6,4 @@ type UserRepository interface {
 	SaveUser(*entity.User) (*entity.User, error)
 	GetUserByID(id entity.UserID) (*entity.User, error)
 	GetUserByEmail(email string) (*entity.User, error)
-	GetIDByPublicID(publicID entity.UserPublicID) (entity.UserID, error)
-	GetPublicIDByID(id entity.UserID) (entity.UserPublicID, error)
 }
