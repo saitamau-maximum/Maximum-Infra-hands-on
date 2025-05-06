99611ddbe312bb585ab562856e3d88a8050c2de3
nakamuraitsuki <ituki1911n@gmail.com>
2025-05-06 22:05:09 +0900

    refactor(backend/cacheImpl): dtoを隔離

--- backend/internal/infrastructure/serviceImpl/messageCacheImpl/memcached/messageCacheImpl.go
+++ backend/internal/infrastructure/serviceImpl/messageCacheImpl/memcached/messageCacheImpl.go
@@ -12,37 +12,7 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
-// MessageDTO は entity.Message のデータ転送オブジェクトです。
-type MessageDTO struct {
-	ID        string
-	RoomID    string
-	UserID    string
-	Content   string
-	CreatedAt time.Time
-}
 
-func fromEntityMessage(m *entity.Message) *MessageDTO {
-	return &MessageDTO{
-		ID:        string(m.GetID()),
-		RoomID:    string(m.GetRoomID()),
-		UserID:    string(m.GetUserID()),
-		Content:   m.GetContent(),
-		CreatedAt: m.GetSentAt(),
-	}
-}
-
-func toEntityMessage(d *MessageDTO) (*entity.Message, error) {
-	id := entity.MessageID(d.ID)
-	roomID := entity.RoomID(d.RoomID)
-	userID := entity.UserID(d.UserID)
-	return entity.NewMessage(entity.MessageParams{
-		ID:      id,
-		RoomID:  roomID,
-		UserID:  userID,
-		Content: d.Content,
-		SentAt:  d.CreatedAt,
-	}), nil
-}
 
 type messageCache struct {
 	client  *memcache.Client
