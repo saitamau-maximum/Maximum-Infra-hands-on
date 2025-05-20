62e4dab418322252b020c655093f1c0c8c31e536
nakamuraitsuki <isukin@gmail.com>
2025-05-20 17:30:58 +0900

    feat(backend): S3Client作成に必要な環境変数をconfigに追加

--- backend/config/config.go
+++ backend/config/config.go
@@ -8,33 +8,47 @@ import (
 
 // *stringは、nilのときに実装を切り替えるために使う
 type Config struct {
-	Port             string        // サーバーのポート番号
-	CORSOrigin       string        // CORSのオリジン
-	DBPath           string        // SQLite用データベースファイルの場所
-	SecretKey        string        // JWTトークンの署名に使用する秘密鍵
-	HashCost         int           // パスワードハッシュ化に使用するcost値
-	TokenExpiry      time.Duration // JWTトークンの有効期限
-	MySQLDSN         *string       // MySQL用データベースのDSN
-	MemcachedAddr    *string       // Memcachedのアドレス
-	LocalIconDir     string       // ユーザーアイコンのローカル保存先
-	IconStoreBaseURL *string       // ユーザーアイコンの保存先URL
-	IconStoreBucket  *string       // ユーザーアイコンの保存先バケット名
-	IconStorePrefix	 *string       // ユーザーアイコンの保存先プレフィックス
+	Port        string        // サーバーのポート番号
+	CORSOrigin  string        // CORSのオリジン
+	DBPath      string        // SQLite用データベースファイルの場所
+	SecretKey   string        // JWTトークンの署名に使用する秘密鍵
+	HashCost    int           // パスワードハッシュ化に使用するcost値
+	TokenExpiry time.Duration // JWTトークンの有効期限
+	// DB
+	MySQLDSN *string // MySQL用データベースのDSN
+	// Cache
+	MemcachedAddr *string // Memcachedのアドレス
+	// IconStore
+	LocalIconDir       string  // ユーザーアイコンのローカル保存先
+	IconStoreEndpoint  *string // ユーザーアイコンの保存先エンドポイント
+	IconStoreBucket    *string // ユーザーアイコンの保存先バケット名
+	IconStoreAccessKey *string // ユーザーアイコンの保存先アクセスキー
+	IconStoreSecretKey *string // ユーザーアイコンの保存先シークレットキー
+	IconStoreSecure    *string // ユーザーアイコンの保存先セキュアフラグ
+	IconStoreBaseURL   *string // ユーザーアイコンの保存先URL
+	IconStorePrefix    *string // ユーザーアイコンの保存先プレフィックス
 }
 
 func LoadConfig() *Config {
 	return &Config{
-		Port:             getEnv("PORT", "8080"),
-		CORSOrigin:       getEnv("CORS_ORIGIN", "http://localhost:5173"),
-		DBPath:           getEnv("DB_PATH", "database.db"),
-		SecretKey:        getEnv("SECRET_KEY", "secret"),
-		HashCost:         parseInt(getEnv("HASH_COST", "10")),
-		TokenExpiry:      paraseDuration(getEnv("TOKEN_EXPIRY", "24h")),
-		MySQLDSN:         parseStringPointer(getEnv("MYSQL_DSN", "")),
-		MemcachedAddr:    parseStringPointer(getEnv("MEMCACHED_ADDR", "")),
-		LocalIconDir:     getEnv("LOCAL_ICON_DIR", "./images/icons"),
+		Port:        getEnv("PORT", "8080"),
+		CORSOrigin:  getEnv("CORS_ORIGIN", "http://localhost:5173"),
+		DBPath:      getEnv("DB_PATH", "database.db"),
+		SecretKey:   getEnv("SECRET_KEY", "secret"),
+		HashCost:    parseInt(getEnv("HASH_COST", "10")),
+		TokenExpiry: paraseDuration(getEnv("TOKEN_EXPIRY", "24h")),
+		// DB
+		MySQLDSN: parseStringPointer(getEnv("MYSQL_DSN", "")),
+		// Cache
+		MemcachedAddr: parseStringPointer(getEnv("MEMCACHED_ADDR", "")),
+		//IconStore
+		LocalIconDir: getEnv("LOCAL_ICON_DIR", "./images/icons"),
+		IconStoreEndpoint: parseStringPointer(getEnv("ICON_STORE_ENDPOINT", "")),
+		IconStoreBucket:  parseStringPointer(getEnv("ICON_STORE_BUCKET", "")),
+		IconStoreAccessKey: parseStringPointer(getEnv("ICON_STORE_ACCESS_KEY", "")),
+		IconStoreSecretKey: parseStringPointer(getEnv("ICON_STORE_SECRET_KEY", "")),
+		IconStoreSecure:    parseStringPointer(getEnv("ICON_STORE_SECURE", "")),
 		IconStoreBaseURL: parseStringPointer(getEnv("ICON_STORE_BASE_URL", "")),
-		IconStoreBucket: parseStringPointer(getEnv("ICON_STORE_BUCKET", "")),
 		IconStorePrefix:  parseStringPointer(getEnv("ICON_STORE_PREFIX", "")),
 	}
 }
